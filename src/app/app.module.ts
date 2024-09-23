5370929b6d8d7674b2c083770f5fabe2cc715236
guilherme <guico.oliveira3@gmail.com>
2024-09-23 09:07:03 -0300

    fix: tarefas.components.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-
 import { AppComponent } from './app.component';
 import { TarefasComponent } from './components/tarefas/tarefas.component';
 
@@ -20,15 +10,6 @@ import { TarefasComponent } from './components/tarefas/tarefas.component';
     ],
     imports: [
         BrowserModule,
-        HttpClientModule,
-        FormsModule,
-        BrowserAnimationsModule,
-        MatCardModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatButtonModule,
-        MatListModule,
-        MatCheckboxModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/components/tarefas/tarefas.component.ts
+++ src/app/components/tarefas/tarefas.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tarefas',
-  standalone: true,
-  imports: [],
   templateUrl: './tarefas.component.html',
-  styleUrl: './tarefas.component.css'
+  styleUrls: ['./tarefas.component.css']
 })
 export class TarefasComponent {
 
